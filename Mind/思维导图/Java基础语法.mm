348c811c1c62317e5d2a0e1d21280b966130ced7
Ender <end-0@qq.com>
2019-12-10 13:39:57 +0800

    Add files via upload

---
+++ Mind/思维导图/Java基础语法.mm
@@ -0,0 +1,249 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+    <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="01 Java基础语法" >
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="关键字" POSITION="right">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="概述：有特定含义的单词(50)" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="特点：组成关键字的字母全部小写" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="标识符" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="给类、接口、方法、变量起的名字" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="命名规则" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="字母、数字、下划线、和$，不能以数字开头" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="大小写敏感" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="不能跟关键字和保留字（null、true、false）等冲突" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="驼峰标识" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="注释" POSITION="right">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="用于解释说明程序的文字" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="单行注释：//
+多行注释：/**/
+文档注释：/**  */" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="常量" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="在程序执行的过程中其值不发生改变" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="常量分类" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="字面值常量" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="自定义常量" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="变量" POSITION="right">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="在程序执行过程中,在某个范围其值可以发生改变的量" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="变量定义格式：
+数据类型 变量名 = 初始化值；
+注：格式是固定的" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="数据类型和类型转换" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="基本数据类型" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="数值型" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="整数类型（byte，short，int，long）" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="浮点类型（float，double）" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="字符型（char）" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="字符类型" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="布尔型（boolean）" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="引用数据类型" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="类（class）" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="接口（interface）" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="数组[]" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="自动转换条件" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="1. 转换前的数据类型与转换后的类型兼容。
+2. 转换后的数据类型的表示范围比转换前的类型大。" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="强制转换语法：
+目标类型 变量名 = (目标类型)(被转化的数据)" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="运算符" POSITION="right">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="类型" POSITION="right">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="赋值运算符" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="算术运算符" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="关系运算符" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="逻辑运算符" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="条件运算符 " POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="简洁表达式" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="语句" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="概念：" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="流程控制语句分类" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="顺序结构：基础执行方式" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="选择结构：if，if...else及switch语句" POSITION="left">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="循环结构：while，do...while及for语句" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="循环控制" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="循环的中断" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="方法" POSITION="right">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="格式定义" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="方法的重载" POSITION="right">
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="递归调用" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="数组" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="一维数组" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="" POSITION="left">
+        <richcontent TYPE="NODE">
+        <html>
+            <head>
+            </head>
+            <body>
+                <img src="https://kityminder-img.gz.bcebos.com/8c90b25f48bd4ea7e389be01cae73bb3f7ff19e5" width="200" height="97"/>
+            </body>
+        </html>
+    </richcontent>
+</node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="" POSITION="left">
+        <richcontent TYPE="NODE">
+        <html>
+            <head>
+            </head>
+            <body>
+                <img src="https://kityminder-img.gz.bcebos.com/21c91ce5f7c0ee6222275b13a3ec09c4f3fc57ae" width="200" height="94"/>
+            </body>
+        </html>
+    </richcontent>
+</node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="二维数组" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT=" " POSITION="left">
+        <richcontent TYPE="NODE">
+        <html>
+            <head>
+            </head>
+            <body>
+                <img src="https://kityminder-img.gz.bcebos.com/04f3899bd31b514cf1fa705a9463698bcd518a2e" width="200" height="35"/>
+            </body>
+        </html>
+    </richcontent>
+</node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="静态初始化格式" POSITION="left">
+        <richcontent TYPE="NODE">
+        <html>
+            <head>
+            </head>
+            <body>
+                <img src="https://kityminder-img.gz.bcebos.com/0dcf162f1844bddaafb4b7199e11c647f8255f35" width="200" height="10"/>
+            </body>
+        </html>
+    </richcontent>
+</node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="多维数组" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="只要在声明数组时将索引与中括号再加一组即可，以此类推" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="数组的引用传递" POSITION="left">
+            <node CREATED="1573982936315" ID="ID_03b7e96a12fada4a4d4a0935fdcdf492" MODIFIED="1573982936315" TEXT="传递及返回数组" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+    </node>
+
+</map>
---
+++ Mind/思维导图/面向对象.mm
@@ -0,0 +1,63 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+    <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="02 面向对象" >
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="面向对象与面向过程概述" POSITION="right">
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="面向过程（Procedure Oriented 简称PO：如C语言）" POSITION="right">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="面向对象（Object Oriented简称OO：如C++，Java等语言）" POSITION="right">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="面向对象思想的特点" POSITION="right">
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="优点：易维护、易复用、易扩展，由于面向对象有封闭、继承、多态性，可以设计出低耦合的系统，使系统更加灵活、更加易于维护。" POSITION="right">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="缺点：性能比面向过程低" POSITION="right">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="类和对象的关系" POSITION="left">
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="类：是一组相关的属性和行为的集合" POSITION="left">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="对象：是该类事物的具体体现" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="类的定义" POSITION="right">
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="格式" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="类的对象创建与 使用" POSITION="left">
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="对象的创建格式" POSITION="left">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="创建多个对象" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="封装性" POSITION="right">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="构造方法" POSITION="left">
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="定义：类名 对象名称 = new 类名();  //一步到位" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="匿名对象" POSITION="right">
+    </node>
+
+            <node CREATED="1573983003416" ID="ID_74d0e1ae7bcceabe79402025d94edeb8" MODIFIED="1573983003416" TEXT="对象的引用传递" POSITION="left">
+    </node>
+
+    </node>
+
+</map>
